3724990579951247626f40c4ef441e403068ee11
phy <arthur.longuefosse@gmail.com>
2024-05-27 11:42:11 +0000

    revert setup to initial name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_namespace_packages
 
-setup(name='dynamic_network_architectures_translation',
-      packages=find_namespace_packages(include=["dynamic_network_architectures_translation", "dynamic_network_architectures_translation.*"]),
+setup(name='dynamic_network_architectures',
+      packages=find_namespace_packages(include=["dynamic_network_architectures", "dynamic_network_architectures.*"]),
       version='0.3.1',
       description='none',
       author='Fabian Isensee',
